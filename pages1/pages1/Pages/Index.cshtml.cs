d8cb57d632ada10d29afb73772f0ba02a4abd5ca
Alexandr <sashkasashkin1945@gmail.com>
2022-09-14 16:01:56 +0300

    1

---
+++ pages1/pages1/Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace pages1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public string Message { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Message = "Щелконогов Олександр група К19-2";
+        }
+    }
+}
